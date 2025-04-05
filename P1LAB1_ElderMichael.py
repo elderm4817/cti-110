e5d9d52fd59bde1b7fcd4c654f9a4617db20c03f
elderm4817 <elderm4817@student.faytechcc.edu>
2025-04-05 13:47:04 -0400

    Add files via upload
    First Submission.

---
+++ P1LAB1_ElderMichael.py
@@ -0,0 +1,11 @@
+# Michael Elder
+# 04/05/2025
+# P1LAB1
+# This project is to demonstrate the use of the print and input function by asking the user to input their names and then being greated once done.
+
+
+first_name = input("Enter is your first name: ")
+last_name = input("Enter is your last name: ")
+
+print("Hello,", first_name, last_name + '!' , "Welcome to CTI-110")
+
